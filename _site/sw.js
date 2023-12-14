d4211666b4097185481091cf0a44d5582ae35fc5
Marcel Schmitt <schmitt.marcel97@gmail.com>
2023-12-14 21:23:44 +0100

    Add baseurl again

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702583199';
+const cacheName = 'chirpy-1702585401';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
